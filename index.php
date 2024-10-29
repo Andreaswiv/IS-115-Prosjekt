0bfe8a3d92c118d6ff6ec71ec4c84de618fe49b3
Olai Bergh Oldeide <112626308+Olaibo@users.noreply.github.com>
2024-10-29 11:41:02 +0100

    starter prosjekt

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Page</title>
+</head>
+<body>
+    <h3>Motell-booking</h3>
+
+    <ul>
+        <li><a target= "_blank" href="./modul4_oppg1.php"></a></li>
+        <li><a target= "_blank" href="./modul4_oppg2.php"></a></li>
+        <li><a target= "_blank" href="./modul4_oppg3.php"></a></li>
+        <li><a target= "_blank" href="./modul4_oppg4.php"></a></li>
+        <li><a target= "_blank" href="./modul4_oppg5.php"></a></li>
+    </ul>
+</body>
+</html>
