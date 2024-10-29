1e898b5add0b582174be6a4b94211c638e110897
Olai Bergh Oldeide <112626308+Olaibo@users.noreply.github.com>
2024-10-29 12:47:07 +0100

    oppdaterte filbaner

--- index.php
+++ index.php
@@ -9,11 +9,10 @@
     <h3>Motell-booking</h3>
 
     <ul>
-        <li><a target= "_blank" href="./modul4_oppg1.php"></a></li>
-        <li><a target= "_blank" href="./modul4_oppg2.php"></a></li>
+        <li><a target= "_blank" href="./form_nyBruker.php"></a></li>
+        <li><a target= "_blank" href="./form_exisBruker.php"></a></li>
         <li><a target= "_blank" href="./modul4_oppg3.php"></a></li>
         <li><a target= "_blank" href="./modul4_oppg4.php"></a></li>
-        <li><a target= "_blank" href="./modul4_oppg5.php"></a></li>
     </ul>
 </body>
 </html>
