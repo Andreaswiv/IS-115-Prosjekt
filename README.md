35d576f13a3102dceead43d025d47b560658fb82
andreaswiv <andreaswiv@uia.no>
2024-12-02 23:15:28 +0100

    Fikset README + litt til

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 
 Dette er prosjektet til Andreas Wahl Iversen og Olai Bergh Oldeide i IS-115, Webprogrammering i PHP.
 
-Vi har valgt å lage et rombookingsystem for et hotell.
+Vi har valgt å lage et rombookingsystem for et motell.
 
+For å initialisere databasen, importer motel_booking.sql inn i phpMyAdmin.
+
+Sørg for at IS-115-Prosjektet ligger direkte i HTDOCS hvis ikke vil ikke filbanene bli rett.
+
+Stylingen, testdata og javascript er hentet direkte fra ChatGPT.
+
+All koden er vår egen.
--- src/forms/form_allCustomersAdmin.php
+++ src/forms/form_allCustomersAdmin.php
@@ -13,50 +13,45 @@ $userModel = new User($conn);
 
 // Hent alle brukere med rollen "user"
 $customers = $userModel->getUsersByRole('user');
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="no">
 <head>
     <meta charset="UTF-8">
     <title>Kundeoversikt</title>
-    <link rel="stylesheet" href="../../public/assets/css/style.css">
+    <link rel="stylesheet" href="../../public/assets/css/style.css?v1.0.3">
 </head>
 <body>
-<div class="container">
+<div class="allCustomers-container">
     <h1>Kundeoversikt</h1>
     <table>
         <thead>
-            <tr>
-                <th>Bruker-ID</th>
-                <th>Navn</th>
-                <th>Email</th>
-                <th>Telefon</th>
-                <th>Handling</th>
-            </tr>
+        <tr>
+            <th>Bruker-ID</th>
+            <th>Navn</th>
+            <th>Email</th>
+            <th>Telefon</th>
+            <th>Handling</th>
+        </tr>
         </thead>
         <tbody>
-            <?php foreach ($customers as $customer): ?>
-                <tr>
-                    <td><?php echo htmlspecialchars($customer['id']); ?></td>
-                    <td><?php echo htmlspecialchars($customer['firstName'] . ' ' . $customer['lastName']); ?></td>
-                    <td><?php echo htmlspecialchars($customer['email']); ?></td>
-                    <td><?php echo htmlspecialchars($customer['phone']); ?></td>
-                    <td>
-                    <td>
-                        <form action="form_newBooking.php" method="post">
-                            <input type="hidden" name="user_id" value="<?php echo htmlspecialchars($customer['id']); ?>">
-                            <input type="hidden" name="firstName" value="<?php echo htmlspecialchars($customer['firstName']); ?>">
-                            <input type="hidden" name="lastName" value="<?php echo htmlspecialchars($customer['lastName']); ?>">
-                            <button type="submit">Lag en booking</button>
-                        </form>
-                    </td>
-
-                    </td>
-                </tr>
-            <?php endforeach; ?>
+        <?php foreach ($customers as $customer): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($customer['id']); ?></td>
+                <td><?php echo htmlspecialchars($customer['firstName'] . ' ' . $customer['lastName']); ?></td>
+                <td><?php echo htmlspecialchars($customer['email']); ?></td>
+                <td><?php echo htmlspecialchars($customer['phone']); ?></td>
+                <td>
+                    <form action="form_newBooking.php" method="post">
+                        <input type="hidden" name="user_id" value="<?php echo htmlspecialchars($customer['id']); ?>">
+                        <input type="hidden" name="firstName" value="<?php echo htmlspecialchars($customer['firstName']); ?>">
+                        <input type="hidden" name="lastName" value="<?php echo htmlspecialchars($customer['lastName']); ?>">
+                        <button type="submit">Lag en booking</button>
+                    </form>
+                </td>
+            </tr>
+        <?php endforeach; ?>
         </tbody>
     </table>
 </div>
 </body>
 </html>
-
